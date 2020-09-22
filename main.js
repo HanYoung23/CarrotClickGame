ed812598270c0d1ebed02dcf8eb67f8783b6ab72
HanYoung23 <67942048+HanYoung23@users.noreply.github.com>
2020-09-23 00:03:32 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
-// 벌레, 당근 클릭 기능
 // 당근 카운트 + 출력
+
 const header = document.querySelector(".header");
+const headerBtn = document.querySelector(".headerBtn");
 const playBtn = document.querySelector(".fa-play");
-const pauseBtn = document.querySelector(".fa-search");
+//const pauseBtn = document.querySelector(".fa-square");
 const timerAlarm = document.querySelector(".timer");
 const counter = document.querySelector(".carrotCounter");
 const field = document.querySelector(".field");
-const alert = document.querySelector(".alert");
-const replayBtn = document.querySelector(".fa-redo-alt");
+const resultAlert = document.querySelector(".alert");
+//const replayBtn = document.querySelector(".fa-redo-alt");
+//const imgList = document.querySelector(".imgList");
 const message = document.querySelector(".message");
-const bugImg = document.querySelectorAll(".bugImg");
-const carrotImg = document.querySelectorAll(".carrotImg");
+const bugImg = document.querySelector(".bugImg");
+const carrotImg = document.querySelector(".carrotImg");
 
 // 게임 실행 시 기능
 function playGame() {
+  removeImgs();
   changeBtn(); // 플레이 버튼 사라지고 정지버튼 출력
-  countDown(); // 카운트 다운 + 카운트 끝나면 패배 창 출력////
+  //countDown(); // 카운트 다운 + 카운트 끝나면 패배 창 출력////
   creatImgs(); // 이미지들 출력 + 랜덤 기능 추가 ////
   //carrotCount(); // 당근 갯수 파악 + 0되면 승리 창 출력 ////
+  let time = 5;
+  const count = setInterval(() => {
+    if (time > -1) {
+      timerAlarm.innerText = `0 : ${time}`;
+    } else if (time === -1) {
+      displayMessage(`YOU LOST 🤣`);
+      clearInterval(count);
+    }
+    time--;
+  }, 1000);
 }
 
 // 플레이 버튼 동작
@@ -25,36 +38,53 @@ playBtn.addEventListener("click", playGame);
 
 // 플레이 버튼 교체
 function changeBtn() {
-  header.insertAdjacentHTML("afterbegin", `<i class="fas fa-square"></i>`);
-  playBtn.style.display = "none";
+  headerBtn.insertAdjacentHTML("afterbegin", `<i class="fas fa-square"></i>`);
+  playBtn.remove();
 }
 
 // 타이머 기능(출력)
-function countDown() {
-  let i = 10;
-  (function count() {
-    timerAlarm.innerText = `0 : ${i}`;
-    if (i > 0) {
-      setTimeout(count, 1000);
-    }
-    i--;
-  })();
-}
-
+// 알림 창 점멸 시
+//function countDown() {
+/*
+let time = 5;
+const count = setInterval(() => {
+  if (time > -1) {
+    timerAlarm.innerText = `0 : ${time}`;
+  } else if (time === -1) {
+    displayMessage(`YOU LOST 🤣`);
+    clearInterval(count);
+  }
+  time--;
+}, 1000);
+*/
+//}
+//
+//
 // 벌레, 당근 이미지 생성 + 랜덤 배치 기능
 function creatImgs() {
+  const divRow = document.createElement("div");
+  divRow.setAttribute("class", "imgList");
+  field.appendChild(divRow);
   for (let i = 0; i < 10; i++) {
     if (i < 7) {
-      field.insertAdjacentHTML(
+      divRow.insertAdjacentHTML(
         "afterbegin",
         `<img class="bugImg" src="img/bug.png" data-id="${i}"/>`
       );
       randomCoord("bugImg", i);
+      const bugImg = document.querySelector(".bugImg");
+      if (bugImg) {
+        bugImg.addEventListener("click", bugClicked);
+      }
     }
-    field.insertAdjacentHTML(
+    divRow.insertAdjacentHTML(
       "afterbegin",
       `<img class="carrotImg" src="img/carrot.png" data-id="${i}"/>`
     );
+    const carrotImg = document.querySelector(".carrotImg");
+    if (carrotImg) {
+      carrotImg.addEventListener("click", carrotClicked);
+    }
     randomCoord("carrotImg", i);
   }
 }
@@ -62,11 +92,6 @@ function randomCoord(imgName, i) {
   const element = document.querySelector(`.${imgName}[data-id="${i}"]`);
   element.style.left = Math.round(Math.random() * field.offsetWidth) + "px";
   element.style.top = Math.round(Math.random() * field.offsetHeight) + "px";
-}
-
-// 각 이미지들에 속성 부여
-function bugclicked() {
-  console.log("asdf");
 }
 
 // 메세지 출력 창 + 리플레이 버튼 기능
@@ -80,7 +105,34 @@ function displayMessage(message) {
   );
   const replayBtn = document.querySelector(".fa-redo-alt");
   if (replayBtn) {
-    replayBtn.addEventListener("click", playGame, false);
+    replayBtn.addEventListener("click", () => {
+      replayBtn.parentNode.remove();
+      playGame();
+    });
+  }
+  const pauseBtn = document.querySelector(".fa-square");
+  if (pauseBtn) {
+    pauseBtn.remove();
   }
 }
-displayMessage("You Win or lost");
+
+// 벌레 클릭 기능
+function bugClicked() {
+  displayMessage(`YOU LOST 🤣`);
+  this.remove();
+}
+
+// 당근 클릭 기능
+function carrotClicked() {
+  console.log("asdf");
+}
+
+// 기존 벌레, 당근 이미지 제거
+function removeImgs() {
+  const imgList = document.querySelector(".imgList");
+  if (imgList) {
+    imgList.remove();
+  }
+}
+
+// 타이머 멈추기 부터
